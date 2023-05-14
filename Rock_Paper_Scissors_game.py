c99b7a74eb7c9aae9409bcc6ac47fe9e9acf5cff
khettabiSoufiane <soufiane97khettabi@gmail.com>
2023-05-14 16:41:04 +0100

    Rock Paper Scissors game

---
+++ Rock_Paper_Scissors_game.py
@@ -0,0 +1,35 @@
+import random
+
+# start Rock Paper Scissors game
+print("The game has started, let's go!")
+
+while True:
+    
+        # user input: r, p, s
+        user = input('insert your option " Rock : r ", " Paper : p ", " Scissors : s ": ')
+
+        # import random library
+        pc = random.choice(["r", "p", "s"])
+
+        # Building logic game
+
+        print(f"Player is : {user}")
+        print(f"Pc is : {pc}")
+        
+        if user == "r" and pc == "s" or user == "p" and pc =="r" or user == "s" and pc == "p":
+            print("is very good, you win")
+
+        elif user == "s" and pc == "r" or user == "r" and pc =="p" or user == "p" and pc == "s":
+            print("I'm sorry, pc won")
+
+        elif user == pc:
+            print("it's a tie")
+
+        else :
+            print('please insert your option " Rock : r ", " Paper : p ", " Scissors : s".' )
+
+        repeat = input("Do you want to continue(y/n): ")
+        if repeat == "n":
+            break
+
+print("Game exited")
